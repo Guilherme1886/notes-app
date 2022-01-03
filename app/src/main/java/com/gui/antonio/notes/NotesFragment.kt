56b218ebcb4b81d16b89bacde08d914652966d46
Guilherme Toledo <guilherme.jose95@hotmail.com>
2022-01-03 06:29:18 -0300

    add safe args

--- app/src/main/java/com/gui/antonio/notes/NotesFragment.kt
+++ app/src/main/java/com/gui/antonio/notes/NotesFragment.kt
@@ -1,6 +1,7 @@
 package com.gui.antonio.notes
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,69 +18,72 @@ class NotesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notesBinding = FragmentNotesBinding.inflate(inflater, container, false)
-        notesBinding?.notesRecyclerView?.apply {
-            setHasFixedSize(true)
-            notesAdapter = NotesAdapter().apply {
-                submitList(getNotes())
-                onClickNote = {
-                    findNavController().navigate(R.id.action_notesFragment_to_noteDetailsFragment)
-                }
-                onDeleteNote = { note, position ->
-                    val action = NoteModalBottomSheetAction.Removing(
-                        note = NoteModalBottomSheetModel(
-                            title = note.title,
-                            description = note.description
+        if (notesBinding == null) {
+            notesBinding = FragmentNotesBinding.inflate(inflater, container, false)
+            notesBinding?.notesRecyclerView?.apply {
+                setHasFixedSize(true)
+                notesAdapter = NotesAdapter().apply {
+                    submitList(getNotes())
+                    onClickNote = {
+                        findNavController().navigate(
+                            NotesFragmentDirections.actionNotesFragmentToNoteDetailsFragment()
                         )
-                    )
-                    val modal = ModalBottomSheet(action = action) {
-                        val b = getNotes()
-                        b.removeAt(position)
-                        notesAdapter?.submitList(b)
                     }
-                    modal.show(parentFragmentManager, ModalBottomSheet.TAG)
-                }
-                onEditNote = {
-                        note, position ->
-                    val action = NoteModalBottomSheetAction.Editing(
-                        note = NoteModalBottomSheetModel(
-                            title = note.title,
-                            description = note.description
-                        )
-                    )
-                    val modal = ModalBottomSheet(action = action) {
-                        val b = getNotes()
-                        b.removeAt(position)
-                        b.add(position, it)
-                        notesAdapter?.submitList(b)
-                    }
-                    modal.show(parentFragmentManager, ModalBottomSheet.TAG)
-                }
-            }
-            adapter = notesAdapter
-        }
-        notesBinding?.notesAddFloatingActionButton?.setOnClickListener {
-            val modal = ModalBottomSheet(action = NoteModalBottomSheetAction.Adding) {
-                val b = getNotes()
-                notesAdapter?.submitList(
-                    b.apply {
-                        add(
-                            NotePresentation(
-                                title = it.title,
-                                description = it.description
+                    onDeleteNote = { note, position ->
+                        val action = NoteModalBottomSheetAction.Removing(
+                            note = NoteModalBottomSheetModel(
+                                title = note.title,
+                                description = note.description
                             )
                         )
+                        val modal = ModalBottomSheet(action = action) {
+                            val b = getNotes()
+                            b.removeAt(position)
+                            notesAdapter?.submitList(b)
+                        }
+                        modal.show(parentFragmentManager, ModalBottomSheet.TAG)
                     }
-                )
+                    onEditNote = { note, position ->
+                        val action = NoteModalBottomSheetAction.Editing(
+                            note = NoteModalBottomSheetModel(
+                                title = note.title,
+                                description = note.description
+                            )
+                        )
+                        val modal = ModalBottomSheet(action = action) {
+                            val b = getNotes()
+                            b.removeAt(position)
+                            b.add(position, it)
+                            notesAdapter?.submitList(b)
+                        }
+                        modal.show(parentFragmentManager, ModalBottomSheet.TAG)
+                    }
+                }
+                adapter = notesAdapter
             }
-            modal.show(parentFragmentManager, ModalBottomSheet.TAG)
+            notesBinding?.notesAddFloatingActionButton?.setOnClickListener {
+                val modal = ModalBottomSheet(action = NoteModalBottomSheetAction.Adding) {
+                    val b = getNotes()
+                    notesAdapter?.submitList(
+                        b.apply {
+                            add(
+                                NotePresentation(
+                                    title = it.title,
+                                    description = it.description
+                                )
+                            )
+                        }
+                    )
+                }
+                modal.show(parentFragmentManager, ModalBottomSheet.TAG)
+            }
         }
-
         return notesBinding?.root
     }
 
     override fun onDestroyView() {
-        notesBinding = null
+        Log.d("NotesFragment", "onDestroyView")
+//        notesBinding = null
         super.onDestroyView()
     }
 
